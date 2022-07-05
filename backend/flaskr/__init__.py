02ebc11820147a9739a17f3349729f6b864ebf76
SimzMapisa <simbarashe795@gmail.com>
2022-07-05 08:35:35 +0200

    Documented all endpoints

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,3 +1,4 @@
+from functools import total_ordering
 import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -47,10 +48,31 @@ def create_app(test_config=None):
     Create an endpoint to handle GET requests
     for all available categories.
     """
-    @app.route('/categories')
+    @app.route('/api/v1/categories')
     def retrieve_categories():
+
+        # - Method Get
+        # - Endpoint - /api/v1/categories
+        # - Fetches a dictionary of categories in which the keys are the ids and the value is the corresponding string of the category
+        # - Request Arguments: None
+        # - Returns: An object with a single key, `categories`, that contains an object of `id: category_string` key: value pairs.
+        ###Sample result##
+        # {
+        #     "categories": {
+        #         "1": "Science",
+        #         "2": "Art",
+        #         "3": "Geography",
+        #         "4": "History",
+        #         "5": "Entertainment",
+        #         "6": "Sports"
+        #     },
+        #     "success": true,
+        # }
+
         categories = Category.query.all()
         categories_dict = {}
+
+        # condition to check if there are categories
         if categories is None:
             return not_found(404)
 
@@ -66,19 +88,59 @@ def create_app(test_config=None):
     including pagination (every 10 questions).
     This endpoint should return a list of questions,
     number of total questions, current category, categories.
-    
+
     TEST: At this point, when you start the application
     you should see questions and categories generated,
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
 
-    @app.route('/questions')
+    @app.route('/api/v1/questions')
     def retrieve_questions():
+
+        # - Method Get
+        # - Endpoint - /api/v1/questions
+        # - Fetches a dictionary of categories and a list of questions
+        # - Request Arguments: None
+        # - Both the dictionary of categories and the list of questions have key value pairs with the id as the key and the string value as the value
+        ###Sample result##
+        # {
+        #     "categories": {
+        #         "1": "Science",
+        #         "2": "Art",
+        #         "3": "Geography",
+        #         "4": "History",
+        #         "5": "Entertainment",
+        #         "6": "Sports"
+        #     },
+        #     "current_category": null,
+        #     "questions": [
+        #             {
+        #                 "answer": "Apollo 13",
+        #                 "category": 5,
+        #                 "difficulty": 4,
+        #                 "id": 2,
+        #                 "question": "What movie earned Tom Hanks his third straight Oscar nomination, in 1996?"
+        #             }
+        #         ],
+        #   "success": true,
+        #   "total_questions": 21
+        # }
+
         selection = Question.query.order_by(Question.id).all()
         current_questions = paginate_questions(request, selection)
         categories = Category.query.all()
         categories_dict = {}
+
+        # condition to check if the server has any questions
+        if len(current_questions) == 0:
+            return not_found(404)
+
+        # condition to check if the server is running
+        if selection is None:
+            return server_error(500)
+
+        # condition to check if the server has any categories
         if categories is None:
             return not_found(404)
         for category in categories:
@@ -99,8 +161,32 @@ def create_app(test_config=None):
     This removal will persist in the database and when you refresh the page.
     """
 
-    @app.route('/questions/<int:question_id>', methods=['DELETE'])
+    @app.route('/api/v1/questions/<int:question_id>', methods=['DELETE'])
     def delete_question(question_id):
+
+        # - Method Delete
+        # - Returns status code 200
+        # - Endpoint - /api/v1/questions/<int:question_id>
+        # - Fetches a question by id and deletes it
+        # - Request Arguments: None
+        # - Returns a success message indicating a successful deletion, updates the number of questions and returns the list of questions
+        ###Sample result##
+        # {
+        #     "deleted": 18,
+        #     "message": "Question deleted successfully.",
+        #     "questions": [
+        #         {
+        #             "answer": "Apollo 13",
+        #             "category": 5,
+        #             "difficulty": 4,
+        #             "id": 2,
+        #             "question": "What movie earned Tom Hanks his third straight Oscar nomination, in 1996?"
+        #         }
+        #     ],
+        #     "success": True
+        #     "total_questions": 20
+        # }
+
         try:
             question = Question.query.filter(
                 Question.id == question_id).one_or_none()
@@ -112,6 +198,7 @@ def create_app(test_config=None):
 
             return jsonify({
                 'success': True,
+                'status_code': 200,
                 'deleted': question_id,
                 'message': 'Question deleted successfully.',
                 'questions': current_questions,
@@ -130,13 +217,24 @@ def create_app(test_config=None):
     the form will clear and the question will appear at the end of the last page
     of the questions list in the "List" tab.
     """
-    @app.route('/questions', methods=['POST'])
+    @app.route('/api/v1/questions', methods=['POST'])
     def create_question():
+
+        # - Method POST
+        # - Returns status code 200
+        # - Endpoint - /api/v1/questions
+        # - Creates a new question, and answer then update the total_questions
+        # - Request Arguments: CONTENT_TYPE : application/json
+        # - Returns a success message indicating a successful question creation
+
         body = request.get_json()
         new_question = body.get('question', None)
         new_answer = body.get('answer', None)
         new_difficulty = body.get('difficulty', None)
         new_category = body.get('category', None)
+
+        if new_question is None or new_answer is None or new_difficulty is None or new_category is None:
+            return unprocessable(422)
         try:
             question = Question(question=new_question, answer=new_answer,
                                 difficulty=new_difficulty, category=new_category)
@@ -164,8 +262,16 @@ def create_app(test_config=None):
     only question that include that string within their question.
     Try using the word "title" to start.
     """
-    @app.route('/questions/search', methods=['POST'])
+    @app.route('/api/v1/questions/search', methods=['POST'])
     def search_questions():
+
+        # - Method POST
+        # - Returns status code 200
+        # - Endpoint - /api/v1/questions/search
+        # - Queries the database searching for questions based on search term
+        # - Request Arguments: None
+        # - Returns returns questions that match the search term
+
         body = request.get_json()
         search_term = body.get('searchTerm', None)
         selection = Question.query.filter(
@@ -194,8 +300,16 @@ def create_app(test_config=None):
     category to be shown.
     """
 
-    @app.route('/categories/<int:category_id>/questions')
+    @app.route('/api/v1/categories/<int:category_id>/questions')
     def retrieve_questions_by_category(category_id):
+
+        # - Method GET
+        # - Returns status code 200
+        # - Endpoint - /api/v1/categories/<int:category_id>/questions
+        # - Fetches a question based on the category_id
+        # - Request Arguments: None
+        # - Returns all questions associated with the returned category
+
         selection = Question.query.filter(
             Question.category == category_id).all()
         current_questions = paginate_questions(request, selection)
@@ -224,8 +338,16 @@ def create_app(test_config=None):
     one question at a time is displayed, the user is allowed to answer
     and shown whether they were correct or not.
     """
-    @app.route('/quizzes', methods=['POST'])
+    @app.route('/api/v1/quizzes', methods=['POST'])
     def play_quiz():
+
+        # - Method POST
+        # - Returns status code 200
+        # - Endpoint - /api/v1/quizzes
+        # - Randomly selects a question from the database
+        # - Request Arguments: None
+        # - Returns one question at a time, the user is allowed to answer
+
         body = request.get_json()
 
         if not ('quiz_category' in body and 'previous_questions' in body):
@@ -272,7 +394,7 @@ def create_app(test_config=None):
         return jsonify({
             "success": False,
             "error": 422,
-            "message": "Querry can not be processed check your infomation and try again"
+            "message": "unprocessable"
         }), 422
 
     @app.errorhandler(400)
@@ -290,5 +412,13 @@ def create_app(test_config=None):
             "error": 405,
             "message": "Method not allowed"
         }), 405
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "Internal server error"
+        }), 500
 
     return app
