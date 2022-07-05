92dc521b6278c4b41f661666f207dd98d8be53cb
SimzMapisa <simbarashe795@gmail.com>
2022-07-05 08:32:49 +0200

    added tests for all endpoints

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -1,4 +1,6 @@
+import difflib
 import os
+from this import d
 import unittest
 import json
 from flask_sqlalchemy import SQLAlchemy
@@ -15,7 +17,9 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://{}:{}@{}/{}".format(
+            "postgres", "atoncemedia2022", "localhost:5432", self.database_name
+        )
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,7 +28,7 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -33,6 +37,75 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
+    def test_get_all_questions(self):
+        res = self.client().get("/api/v1/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(len(data["questions"]))
+
+    def test_get_all_categories(self):
+        res = self.client().get("/api/v1/categories")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["categories"])
+        self.assertTrue(len(data["categories"]))
+
+    def test_get_questions_by_category(self):
+        res = self.client().get("/api/v1/categories/1/questions")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(len(data["questions"]))
+
+    def test_create_question(self):
+        res = self.client().post(
+            "/api/v1/questions",
+            json={"question": "What is the best food?",
+                  "answer": "Pizza", "category": "1", "difficulty": "1"},
+        )
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+
+    def test_search_questions(self):
+        res = self.client().post("/api/v1/questions/search",
+                                 json={"searchTerm": "a"})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertTrue(data["total_questions"])
+        self.assertTrue(len(data["questions"]))
+
+    def test_create_question_with_no_question(self):
+        res = self.client().post(
+            "/api/v1/questions",
+            json={"answer": "Pizza", "category": "1", "difficulty": "1"},
+        )
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "unprocessable")
+
+    def test_create_question_with_no_answer(self):
+        res = self.client().post("/api/v1/questions",
+                                 json={"question": "Which language is best for machine learning?", "category": "5", "difficulty": "4"})
+
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "unprocessable")
 
 
 # Make the tests conveniently executable
